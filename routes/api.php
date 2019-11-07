b33e6125dfe4513a62e57eee334492d5534f9340
lucasmatpie <lucasmatpie@gmail.com>
2019-11-07 00:49:21 +0000

    auth routes  with JWT

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::middleware('auth:api')->group(function ($e){
+
 Route::get('alunos', 'AlunoController@index');
 Route::get('alunos/{aluno}', 'AlunoController@show');
 Route::post('alunos', 'AlunoController@store');
@@ -20,8 +22,17 @@ Route::put('alunos/{aluno}', 'AlunoController@update');
 Route::delete('alunos/{aluno}', 'AlunoController@destroy');
 
 
+
 Route::post('alunos/{aluno}/notas', 'NotasController@store');
 Route::get('alunos/{aluno}/notas', 'NotasController@index');
 Route::get('alunos/{aluno}/notas/{nota}', 'NotasController@show');
 Route::put('alunos/{aluno}/notas/{nota}', 'NotasController@update');
 Route::delete('alunos/{aluno}/notas/{nota}', 'NotasController@destroy');
+
+Route::post('auth/logout', 'AuthController@logout');
+Route::get('auth', 'AuthController@index');
+
+});
+
+Route::post('auth/register', 'AuthController@register');
+Route::post('auth/login', 'AuthController@login');
